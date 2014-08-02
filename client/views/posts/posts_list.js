1282afadc4ec61dfebe6e1a5bc29e74cc352a30d
Stefan Baumgartner <stefanbaumg@gmail.com>
2014-08-02 09:14:07 -0400

    Initial Commit Microscope
    This is the project from the “Discover Meteor” book.

---
+++ client/views/posts/posts_list.js
@@ -0,0 +1,16 @@
+var postsData = [{
+    title: 'Introducing Telescope',
+    author: 'Sacha Greif',
+    url: 'http://sachagreif.com/introducing-telescope/'
+}, {
+    title: 'Meteor',
+    author: 'Tom Coleman',
+    url: 'http://meteor.com'
+}, {
+    title: 'The Meteor Book',
+    author: 'Tome Coleman',
+    url: 'http://temeteorbook.com'
+}];
+Template.postsList.helpers({
+    posts: postsData
+});
