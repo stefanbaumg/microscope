33f054f2740a3930147fd9b08f832d6c8a1a4976
Stefan Baumgartner <stefanbaumg@gmail.com>
2014-08-02 20:11:47 -0400

    Finished Chapter 4

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,16 +1,5 @@
-var postsData = [{
-    title: 'Introducing Telescope',
-    author: 'Sacha Greif',
-    url: 'http://sachagreif.com/introducing-telescope/'
-}, {
-    title: 'Meteor',
-    author: 'Tom Coleman',
-    url: 'http://meteor.com'
-}, {
-    title: 'The Meteor Book',
-    author: 'Tome Coleman',
-    url: 'http://temeteorbook.com'
-}];
 Template.postsList.helpers({
-    posts: postsData
+    posts: function() {
+        return Posts.find();
+    }
 });
