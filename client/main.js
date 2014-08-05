664952cdf141a651c4a4c586d3ab146f6ddb62b8
Stefan Baumgartner <stefanbaumg@gmail.com>
2014-08-04 20:07:37 -0400

    Finished Chapter 6.5

--- client/main.js
+++ client/main.js
@@ -1,1 +1,14 @@
+Meteor.startup(function() {
+    Deps.autorun(function() {
+        console.log('There are ' + Posts.find().count() + ' posts');
+    });
 
+    Posts.find().observe({
+        added: function(post) {
+            console.log('Added post ' + post.title);
+        },
+        changed: function(post) {
+            console.log('Changed post ' + post.title);
+        }
+    });
+});
