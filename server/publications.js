08b956fac81ef870cc9f44a405f3dfa3a7ffd278
Stefan Baumgartner <stefanbaumg@gmail.com>
2014-08-20 14:46:34 -0400

    Finished Chapter 11

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,14 @@ Meteor.publish('posts', function() {
     return Posts.find();
 });
 
-Meteor.publish('comments', function() {
-  return Comments.find();
+Meteor.publish('comments', function(postId) {
+    return Comments.find({
+        postId: postId
+    });
+});
+
+Meteor.publish('notifications', function() {
+    return Notifications.find({
+        userId: this.userId
+    });
 });
